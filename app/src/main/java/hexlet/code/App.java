e804df2ad76e9a99954d9cf08d76e2419e6aaf6e
Peter Pesotskiy <ppeter777@yandex.ru>
2024-05-12 11:17:29 +0300

    1

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,18 @@ package hexlet.code;
 import io.javalin.Javalin;
 
 public class App {
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+    public static void main(String[] args) {
+        var app = getApp()
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(getPort());
+    }
+
     public static Javalin getApp() {
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
