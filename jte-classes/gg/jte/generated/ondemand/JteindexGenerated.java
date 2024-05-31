a37f17b5820bd298268b3c6c17fe4c01699dd61d
Peter Pesotskiy <ppeter777@yandex.ru>
2024-06-01 00:00:06 +0300

    add bootstrap

--- jte-classes/gg/jte/generated/ondemand/JteindexGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/JteindexGenerated.java
@@ -1,9 +1,9 @@
 package gg.jte.generated.ondemand;
 public final class JteindexGenerated {
 	public static final String JTE_NAME = "index.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,0,0,0,16,16,16,16,16,16};
+	public static final int[] JTE_LINE_INFO = {0,0,0,0,0,22,22,22,22,22,22};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
-		jteOutput.writeContent("\n<!doctype html>\n<html lang=\"en\">\n    <head>\n    <meta charset=\"utf-8\" />\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n    <title>Hello Hexlet!</title>\n    </head>\n    <body>\n    <div class=\"col-lg-8 mx-auto p-4 py-md-5\">\n        <main>\n        <h1 class=\"text-body-emphasis\">Привет, Хекслет!</h1>\n        <p>Javalin + jte</p>\n        </main>\n    </div>\n    </body>\n</html>");
+		jteOutput.writeContent("\n<!doctype html>\n<html lang=\"en\">\n<head>\n  <meta charset=\"UTF-8\">\n  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n  <title>Подключение Bootstrap</title>\n\n  <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" crossorigin=\"anonymous\">\n  <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p\" crossorigin=\"anonymous\"></script>\n</head>\n    <body>\n    <div class=\"col-lg-8 mx-auto p-4 py-md-5\">\n        <main>\n        <h1 class=\"text-body-emphasis\">Проект 4</h1>\n        <h2>Анализатор страниц</h2>\n        <p>Четвёртый проект – полноценный веб-сайт на базе фреймворка Javalin.\n        Здесь отрабатываются базовые принципы построения современных сайтов на MVC-архитектуре:\n         работа с роутингом, обработчиками запросов и шаблонизатором, взаимодействие с базой данных через ORM.</p>\n        </main>\n    </div>\n    </body>\n</html>");
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		render(jteOutput, jteHtmlInterceptor);
