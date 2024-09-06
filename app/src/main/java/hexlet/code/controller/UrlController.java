d0c758c25e6f1da9eb5495245ee5f74a16270b18
Peter Pesotskiy <ppeter777@yandex.ru>
2024-09-06 22:25:08 +0300

    add UrlCheckRepository

--- app/src/main/java/hexlet/code/controller/UrlController.java
+++ app/src/main/java/hexlet/code/controller/UrlController.java
@@ -7,6 +7,7 @@ import hexlet.code.model.UrlCheck;
 import hexlet.code.utils.NamedRoutes;
 import hexlet.code.model.Url;
 import hexlet.code.repository.UrlRepository;
+import hexlet.code.repository.UrlCheckRepository;
 import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
 import kong.unirest.HttpResponse;
@@ -28,7 +29,7 @@ public class UrlController {
         var id = ctx.pathParamAsClass("id", Long.class).get();
         var url = UrlRepository.find(id)
                 .orElseThrow(() -> new NotFoundResponse("Url with idd = " + id + " not found"));
-        var urlChecks = UrlRepository.getChecksByUrlId(id);
+        var urlChecks = UrlCheckRepository.getChecksByUrlId(id);
         var page = new UrlPage(url, urlChecks);
         page.setFlash(ctx.consumeSessionAttribute("flash"));
         page.setFlashType(ctx.consumeSessionAttribute("flashType"));
@@ -65,7 +66,7 @@ public class UrlController {
 
     public static void index(Context ctx) throws SQLException {
         var urls = UrlRepository.getEntities();
-        var checks = UrlRepository.getChecks();
+        var checks = UrlCheckRepository.getChecks();
         var page = new UrlsPage(urls, checks);
         page.setFlash(ctx.consumeSessionAttribute("flash"));
         page.setFlashType(ctx.consumeSessionAttribute("flashType"));
@@ -99,7 +100,7 @@ public class UrlController {
             check.setUrlId(id);
             var createdAt = new Timestamp(System.currentTimeMillis());
             check.setCreatedAt(createdAt);
-            UrlRepository.saveCheck(check);
+            UrlCheckRepository.saveCheck(check);
             ctx.sessionAttribute("flash", "Страница успешно проверена");
             ctx.sessionAttribute("flashType", "success");
         } catch (Exception exception) {
---
+++ app/src/main/java/hexlet/code/repository/UrlCheckRepository.java
@@ -0,0 +1,106 @@
+package hexlet.code.repository;
+
+import hexlet.code.model.UrlCheck;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+public class UrlCheckRepository extends BaseRepository{
+
+    public static List<UrlCheck> getChecksByUrlId(long urlId) throws SQLException {
+        String sql = "SELECT * FROM url_checks WHERE url_id = ?";
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            stmt.setLong(1, urlId);
+            var resultSet = stmt.executeQuery();
+            var result = new ArrayList<UrlCheck>();
+            while (resultSet.next()) {
+                var id = resultSet.getInt("id");
+                var statusCode = resultSet.getInt("status_code");
+                var title = resultSet.getString("title");
+                var h1 = resultSet.getString("h1");
+                var description = resultSet.getString("description");
+                var createdAt = resultSet.getTimestamp("created_at");
+                var check = new UrlCheck(id, urlId, statusCode, title, h1, description, createdAt);
+                result.add(check);
+            }
+            Collections.reverse(result);
+            return result;
+        }
+    }
+
+    public static void saveCheck(UrlCheck check) throws SQLException {
+        String sql = "INSERT INTO url_checks (url_id, status_code, title, h1, description, created_at) "
+                + "VALUES (?, ?, ?, ?, ?, ?)";
+        try (var conn = dataSource.getConnection();
+             var preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setLong(1, check.getUrlId());
+            preparedStatement.setLong(2, check.getStatusCode());
+            preparedStatement.setString(3, check.getTitle());
+            preparedStatement.setString(4, check.getH1());
+            preparedStatement.setString(5, check.getDescription());
+            preparedStatement.setTimestamp(6, check.getCreatedAt());
+            preparedStatement.executeUpdate();
+            var generatedKeys = preparedStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                check.setId(generatedKeys.getLong(1));
+            } else {
+                throw new SQLException("DB have not returned an id after saving an entity");
+            }
+        }
+    }
+
+    public static Map<Long, UrlCheck> getLastChecks() throws SQLException {
+        String sql = "SELECT DISTINCT ON (url_id) * FROM url_checks ORDER BY created_at DESC";
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            var resultSet = stmt.executeQuery();
+            var result = new HashMap<Long, UrlCheck>();
+            while (resultSet.next()) {
+                var id = resultSet.getInt("id");
+                var statusCode = resultSet.getInt("status_code");
+                var title = resultSet.getString("title");
+                var h1 = resultSet.getString("h1");
+                var description = resultSet.getString("description");
+                var createdAt = resultSet.getTimestamp("created_at");
+                var urlId = resultSet.getLong("url_id");
+                var check = new UrlCheck(id, urlId, statusCode, title, h1, description, createdAt);
+                result.put(urlId, check);
+            }
+            return result;
+        }
+    }
+
+    public static List<UrlCheck> getChecks() throws SQLException {
+        String sql = "SELECT * FROM url_checks";
+        try (var conn = dataSource.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
+            var resultSet = stmt.executeQuery();
+            var result = new ArrayList<UrlCheck>();
+            while (resultSet.next()) {
+                var id = resultSet.getInt("id");
+                var statusCode = resultSet.getInt("status_code");
+                var title = resultSet.getString("title");
+                var h1 = resultSet.getString("h1");
+                var description = resultSet.getString("description");
+                var urlId = resultSet.getLong("url_id");
+                var createdAt = resultSet.getTimestamp("created_at");
+                var check = new UrlCheck();
+                check.setStatusCode(statusCode);
+                check.setTitle(title);
+                check.setH1(h1);
+                check.setDescription(description);
+                check.setCreatedAt(createdAt);
+                check.setId(id);
+                check.setUrlId(urlId);
+                result.add(check);
+            }
+            Collections.reverse(result);
+            return result;
+        }
+    }
+}
