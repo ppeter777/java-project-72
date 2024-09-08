f670d43e0dcc5afd59c78fd2c6cace8a7eae9f32
Peter Pesotskiy <ppeter777@yandex.ru>
2024-09-09 00:40:06 +0300

    linter corr

--- app/src/main/java/hexlet/code/dto/UrlsPage.java
+++ app/src/main/java/hexlet/code/dto/UrlsPage.java
@@ -1,13 +1,9 @@
 package hexlet.code.dto;
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-
 import hexlet.code.model.Url;
 import hexlet.code.model.UrlCheck;
-import hexlet.code.repository.UrlRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
